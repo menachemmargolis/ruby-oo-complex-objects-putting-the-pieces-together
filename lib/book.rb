85afe64526cde44f57f4d00a9a8f6263f0c0d9bc
menachem margolis <margolismendel5728@gmail.com>
2020-12-12 18:17:19 -0500

    commit tree 51f2ef4cf2ab709e362450ecfd705a23d2f97228

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
+require 'pry'
+class Book
+#attr_reader :title
+    def initialize(title_par)
+        @title = title_par
+    end
 
+    def title
+        @title
+    end
+
+    def author=(author_par)
+        @author = author_par
+    end
+
+    def author
+        @author
+    end
+
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def genre
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+      end
+
+end
 
